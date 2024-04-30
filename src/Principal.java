a8e6501b819af58abe3d7f215a3d5c8638cf2a18
oseiasdfarias <oseias.dfarias@gmail.com>
2024-04-30 10:50:55 -0300

    feat: add project Currency Converter

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,12 @@
+
+import br.com.projetoConversor.menu.Menu;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("OLá, Mundo!!!");
+        Menu meuMenu = new Menu();
+        while (true) {
+            meuMenu.menu();
+            meuMenu.selecinarConversao();
+        }
     }
 }
